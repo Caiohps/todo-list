22e10b5dcaa1aed7f0635b7f3368afd58b8ece05
Caiohps <caiohpsilva@gmail.com>
2022-03-12 10:04:44 -0300

    second commit

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 
 //Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 
@@ -22,7 +24,7 @@ function addTodo(event) {
 
     const todoText = document.createElement('span');
     todoText.innerText= todoInput.value;
-    newTodo.classList.add('todo-item');
+    newTodo.classList.add('todoItem');
     todoDiv.appendChild(todoText);
     
 
@@ -48,18 +50,39 @@ function addTodo(event) {
 
 function deleteCheck (e) {
     const item = e.target;
-    // DELETE TODO
-    if (item.classList[0] === "trash-btn") {
-        const todo = item.parentElement;
+    // DELETE TODO ITEM
+    if (item.classList[0] === "trash-btn") {        
+        const todo = item.parentElement
         //Animation
         todo.classList.add("fall");
-        todo.addEventListener("transitionend", function () {
+        
+        todo.addEventListener('transitionend', function() {
             todo.remove();
         });
     }
-    //CHECK MARK
+    // CHECK MARK
     if(item.classList[0] === "completed-btn") {
         const todo = item.parentElement;
         todo.classList.toggle('completed')
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    console.log(todos)
+    // todos.forEach(function(todo) {
+    //     switch (e.target.value) {
+    //         case "all":
+    //             todo.style.display = 'flex';
+    //             break;
+    //         case "completed":
+    //             console.log(todo)
+    //             if(todo.classList.contains('completed')) {
+    //                 todo.style.display = 'flex';
+    //             }else{
+    //                 todo.style.display = 'none';
+    //             }
+    //     }
+    // })
+
 }
